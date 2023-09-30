edc7300035b9a8541acac8c76c7e49b542bbbdec
John Törnblom <john.tornblom@gmail.com>
2023-09-30 21:44:39 +0200

    cosmetics

--- pt.c
+++ pt.c
@@ -142,7 +142,7 @@ pt_step(int pid) {
 }
 
 
-uint64_t
+static uint64_t
 pt_call(pid_t pid, intptr_t addr,
 	uint64_t arg1, uint64_t arg2, uint64_t arg3,
 	uint64_t arg4, uint64_t arg5, uint64_t arg6) {
