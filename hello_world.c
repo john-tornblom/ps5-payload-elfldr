32cde40d644bc9cacbe595ad5db4c98a2b6e78ff
John Törnblom <john.tornblom@gmail.com>
2023-09-30 15:16:35 +0200

    example: log "Hello world!" to /dev/klog

--- hello_world.c
+++ hello_world.c
@@ -17,6 +17,35 @@ along with this program; see the file COPYING. If not, see
 #include "payload.h"
 
 
+static __attribute__ ((used)) long ptr_syscall;
+
+
+asm(".intel_syntax noprefix\n"
+    ".global syscall\n"
+    ".type syscall @function\n"
+    "syscall:\n"
+    "  mov rax, rdi\n"                      // sysno
+    "  mov rdi, rsi\n"                      // arg1
+    "  mov rsi, rdx\n"                      // arg2
+    "  mov rdx, rcx\n"                      // arg3
+    "  mov r10, r8\n"                       // arg4
+    "  mov r8,  r9\n"                       // arg5
+    "  mov r9,  qword ptr [rsp + 8]\n"      // arg6
+    "  jmp qword ptr [rip + ptr_syscall]\n" // syscall
+    "  ret\n"
+    );
+
+long syscall(long sysno, ...);
+
+
+void
+klog(const char *s) {
+  syscall(0x259, 7, "<118>", 0);
+  syscall(0x259, 7, s, 0);
+  syscall(0x259, 7, "\n", 0);
+}
+
+
 void
 _start(const payload_args_t *args) {
   void (*puts)(const char*);
@@ -24,7 +53,13 @@ _start(const payload_args_t *args) {
   if((args->sceKernelDlsym(0x2, "puts", &puts))) {
     return;
   }
+  if(args->sceKernelDlsym(0x2001, "getpid", &ptr_syscall)) {
+    return;
+  }
+  ptr_syscall += 0xa; // jump directly to syscall instruction
 
+
+  klog("[hello_world.elf] Hello, world!");
   puts("[hello_world.elf] Hello, world!");
 }
 
