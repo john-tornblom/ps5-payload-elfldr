66bab5c9345ead3eaacb67bb82c92db219a344d9
John Törnblom <john.tornblom@gmail.com>
2024-02-27 19:22:09 +0100

    add a stress-test that deploys a simple payload 5000 times, which currently causes kern.ipc.maxpipekva to be exceeded

---
+++ stress-test/stress-test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#   Copyright (C) 2024 John Törnblom
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not see
+# <http://www.gnu.org/licenses/>.
+
+SCRIPTDIR="${BASH_SOURCE[0]}"
+SCRIPTDIR="$(dirname "${SCRIPTDIR}")"
+
+cd $SCRIPTDIR || exit 1
+
+echo "Deploying test.elf 5000 times..."
+sleep 3
+
+for i in {1..5000}
+do
+    make test || exit 1
+done
